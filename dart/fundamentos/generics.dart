e787095c6e1fcbfc91bb544973e0369482fee0e3
abelsalim <abelsalim@proton.me>
2023-12-16 10:38:05 -0300

    [ADD] definição dos generics

---
+++ dart/fundamentos/generics.dart
@@ -0,0 +1,15 @@
+main() {
+  // Tipando os valores da lista 'frutas' como 'String'
+  List<String> frutas = ['banana', 'maça', 'laranja'];
+
+  print(frutas);
+
+  // Tipando as chaves como 'String' e os valores como 'double'
+  Map<String, double> salarios = {
+    'gerente': 19345.78,
+    'vendedor': 16345.78,
+    'estagiário': 600.00
+  };
+
+  print(salarios);
+}
