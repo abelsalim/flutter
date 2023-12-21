6d38a8038558027cfc34b661d587cdda44517dc2
abelsalim <abelsalim@proton.me>
2023-12-21 06:40:52 -0300

    [ADD] desafio classe carro - script de consumo

---
+++ dart/oo/teste/teste_carro.dart
@@ -0,0 +1,27 @@
+import '../modelo/carro.dart';
+
+String minimoMaximo(Carro carro) {
+  var msg = carro.velocidadeAtual! <= 0
+      ? 'Velocidade mínima de ${carro.velocidadeAtual} atingida!'
+      : 'Velocidade máxima de ${carro.velocidadeAtual} atingida';
+
+  return msg;
+}
+
+void main() {
+  Carro carro = Carro(velocidadeAtual: 0);
+
+  while (!carro.estaNoLimite(acelerar: true)) {
+    print('A velocidade atual é de ${carro.acelerar()}');
+  }
+
+  print(minimoMaximo(carro));
+
+  print(''.padRight(35, '-'));
+
+  while (!carro.estaNoLimite(frear: true)) {
+    print('A velocidade atual é de ${carro.frear()}');
+  }
+
+  print(minimoMaximo(carro));
+}
