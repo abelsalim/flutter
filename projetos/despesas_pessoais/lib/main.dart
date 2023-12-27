f7d1a77186d9a5065dd6bcd6df2ed5efc2ed64e2
abelsalim <abelsalim@proton.me>
2023-12-27 19:43:04 -0300

    [ADD] estilização de data, textos e cards

--- projetos/despesas_pessoais/lib/main.dart
+++ projetos/despesas_pessoais/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './models/tansaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(const ExpenseApp());
 
@@ -16,15 +17,21 @@ class MyHomePage extends StatelessWidget {
   final _transactions = [
     Transaction(
       id: 't1',
-      title: 'Conta de Água',
-      value: 59.99,
-      date: DateTime(2023, 12, 27, 18, 35, 00),
+      title: 'Internet',
+      value: 84.99,
+      date: DateTime(2023, 1, 01, 15, 58, 00),
     ),
     Transaction(
       id: 't2',
+      title: 'Conta de Água',
+      value: 59.99,
+      date: DateTime(2023, 12, 12, 18, 35, 50),
+    ),
+    Transaction(
+      id: 't3',
       title: 'Conta de luz',
       value: 287.47,
-      date: DateTime(2023, 12, 27, 18, 35, 00),
+      date: DateTime(2023, 12, 24, 23, 40, 35),
     )
   ];
 
@@ -34,7 +41,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey[100],
+        backgroundColor: const Color.fromRGBO(252, 228, 236, 1),
         title: const Text(
           'Despesas Pessoais',
           style: TextStyle(
@@ -74,17 +81,36 @@ class MyHomePage extends StatelessWidget {
                             ),
                             decoration: BoxDecoration(
                               border: Border.all(
-                                color: Colors.black,
+                                color: const Color.fromRGBO(191, 77, 115, 1),
                                 width: 2,
                               ),
                             ),
                             padding: const EdgeInsets.all(10),
-                            child: Text(tr.value.toString()),
+                            child: Text(
+                              'R\$ ${tr.value.toStringAsFixed(2)}',
+                              style: const TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(191, 77, 115, 1),
+                              ),
+                            ),
                           ),
                           Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(tr.title.toString()),
-                              Text(tr.date.toString()),
+                              Text(
+                                tr.title.toString(),
+                                style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87),
+                              ),
+                              Text(
+                                DateFormat('dd/MM/y - H:m:ss').format(tr.date),
+                                style: const TextStyle(
+                                  color: Colors.black54,
+                                ),
+                              ),
                             ],
                           )
                         ],
