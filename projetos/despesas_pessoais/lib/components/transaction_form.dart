caf6a1bb0717224f63800f7f33ac4e8123680eea
abelsalim <abelsalim@proton.me>
2023-12-30 10:27:56 -0300

    [ADD] tratando submissão e tipos de teclados

--- projetos/despesas_pessoais/lib/components/transaction_form.dart
+++ projetos/despesas_pessoais/lib/components/transaction_form.dart
@@ -10,6 +10,18 @@ class TransactionForm extends StatelessWidget {
     super.key,
   });
 
+  _submitForm() {
+    final title = titleController.text;
+    // Tente converter para double, senão, sete como 0
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -20,12 +32,29 @@ class TransactionForm extends StatelessWidget {
           children: [
             TextField(
               controller: titleController,
+              // onSubmitted serve para confirma a transação com o 'OK' do
+              // teclado, mas ela necessita de um Function que retorne uma
+              // String, portanto podemos criar uma função vazia repassando
+              // apenas o underscore como um valor vazio|nulo|semvalor.
+              // Nota: mesmo sendo um voidFunction, os () tem que ser repassados
+              // no arrow - diferente de que acontece no onPressed
+              onSubmitted: (_) => _submitForm(),
               decoration: const InputDecoration(
-                labelText: 'Descrição sa despesa',
+                labelText: 'Descrição da despesa',
               ),
             ),
             TextField(
               controller: valueController,
+              keyboardType:
+                  // o 'decimal: true' remete a usabilidade do app em IOS
+                  const TextInputType.numberWithOptions(decimal: true),
+              // onSubmitted serve para confirma a transação com o 'OK' do
+              // teclado, mas ela necessita de um Function que retorne uma
+              // String, portanto podemos criar uma função vazia repassando
+              // apenas o underscore como um valor vazio|nulo|semvalor.
+              // Nota: mesmo sendo um voidFunction, os () tem que ser repassados
+              // no arrow - diferente de que acontece no onPressed
+              onSubmitted: (_) => _submitForm(),
               decoration: const InputDecoration(
                 labelText: 'Valor (R\$)',
               ),
@@ -39,13 +68,8 @@ class TransactionForm extends StatelessWidget {
                       const Color.fromRGBO(191, 77, 115, 1),
                     ),
                   ),
+                  onPressed: _submitForm,
                   child: const Text('Nova Transação'),
-                  onPressed: () {
-                    final title = titleController.text;
-                    // Tente converter para double, senão, sete como 0
-                    final value = double.tryParse(valueController.text) ?? 0.0;
-                    onSubmit(title, value);
-                  },
                 ),
               ],
             )
--- projetos/despesas_pessoais/lib/components/transaction_user.dart
+++ projetos/despesas_pessoais/lib/components/transaction_user.dart
@@ -35,42 +35,6 @@ class _TransactionUserState extends State<TransactionUser> {
       value: 287.47,
       date: DateTime(2023, 12, 24, 23, 40, 35),
     ),
-    Transaction(
-      id: 't4',
-      title: 'Internet',
-      value: 84.99,
-      date: DateTime(2023, 1, 01, 15, 58, 00),
-    ),
-    Transaction(
-      id: 't5',
-      title: 'Conta de Água',
-      value: 59.99,
-      date: DateTime(2023, 12, 12, 18, 35, 50),
-    ),
-    Transaction(
-      id: 't6',
-      title: 'Conta de luz',
-      value: 287.47,
-      date: DateTime(2023, 12, 24, 23, 40, 35),
-    ),
-    Transaction(
-      id: 't7',
-      title: 'Internet',
-      value: 84.99,
-      date: DateTime(2023, 1, 01, 15, 58, 00),
-    ),
-    Transaction(
-      id: 't8',
-      title: 'Conta de Água',
-      value: 59.99,
-      date: DateTime(2023, 12, 12, 18, 35, 50),
-    ),
-    Transaction(
-      id: 't9',
-      title: 'Conta de luz',
-      value: 287.47,
-      date: DateTime(2023, 12, 24, 23, 40, 35),
-    )
   ];
 
   _addTransaction(String title, double value) {
